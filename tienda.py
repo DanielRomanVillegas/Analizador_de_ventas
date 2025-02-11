7cf02fab597f4f75ac2905d4f8716390a6096d04
Daniel Román Villegas <danielromanvillegas@outlook.com>
2025-02-11 15:03:34 -0600

    last changes in tienda

--- tienda.py
+++ tienda.py
@@ -5,18 +5,15 @@ import pandas as pd
 # Analizador de Ventas
 
 ### Objetivo
-Crear un programa en Python que analice un conjunto de **datos de ventas de una tienda**. El programa debe realizar varias operaciones de Data Science para proporcionar información valiosa sobre las ventas de la tienda.
+Crear analizador de un conjunto de **datos de ventas de una tienda** realizando varias operaciones de Data Science para proporcionar información valiosa sobre las ventas de la tienda.
 
-### Consigna
-
-1. **Lectura de Datos**: Crea un DataFrame que contenga los datos provistos en el archivo **Datos_Ventas_Tienda.csv** provisto en esta lección. El archivo incluirá información como *fecha de venta*, *categoría de producto*, *cantidad vendida* y *precio*.
-2. **Fusión de Datos**: Crea un segundo DataFrame que contenga los datos del archivo **Datos_Ventas_Tienda2.csv** (también provisto en esta lección), y concaténalos para tener un solo dataFrame con toda la información.
-
-3. **Tratamiento de Datos**: Utiliza Pandas para manipular estos datos. Deberás realizar tareas como limpieza de datos, filtrado y  transformaciones básicas.
-4. **Análisis de Ventas**: Realiza análisis para responder preguntas como:
+1. **Lectura de Datos**
+2. **Fusión de Datos**
+3. **Tratamiento de Datos**
+4. **Análisis de Ventas:** 
 - ¿Cuál es el producto más vendido?
 - ¿Cuál es el mes con más ventas?
-5. **Datos Agrupados**: Agrupa los datos por categoría de producto y analiza las ventas por categoría.
+5. **Datos Agrupados**
 '''
 
 '''## Lectura y fusión de datos'''
